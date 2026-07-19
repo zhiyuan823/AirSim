2b31b54be2ac4acaabd8f66262967a96adf3a324
calmman <calmman.yang@163.com>
2026-07-19 16:40:48 +0800

    修正了工程的配置错误

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+REM call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 @echo off
 REM //---------- set up variable ----------
 setlocal
